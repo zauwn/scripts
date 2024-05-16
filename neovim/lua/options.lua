008a3cf0c3026c672ffe868858b0b04809eb567c
Duarte Rocha <duarte.rocha@kelvininc.com>
2024-05-16 10:23:22 +0100

    removed unused options and comments

--- neovim/lua/options.lua
+++ neovim/lua/options.lua
@@ -2,13 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
-local opt = vim.opt
+-- local opt = vim.opt
 -- o.cursorlineopt ='both' -- to enable cursorline!
 -- enable relative line numbers
 -- opt.relativenumber = true
 --
 --
-
--- Wtf - Openai wtf.nvim plugins
-    -- An alternative way to set your API key
-openai_api_key = "sk-xxxxxxxxxxxxxx"
