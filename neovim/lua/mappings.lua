abfbe407e3290dd0353ed6df49de4002d7a93ab4
Duarte Rocha <duarte.rocha@kelvininc.com>
2024-11-15 19:29:36 +0000

    added gitsigns shortcuts

--- neovim/lua/mappings.lua
+++ neovim/lua/mappings.lua
@@ -6,8 +6,8 @@ local map = vim.keymap.set
 
 -- vim goodies
 -- map("n", ";",  ":",     { desc = "Semi-colon (;) as a colon (:)" })
-map("i", "jk", "<ESC>", { desc = "jk to escape to normal mode" })
-map("n", "<leader>Q", "<cmd>qa<cr>", { desc = "Quit all" })
+map("i", "jk", "<ESC>", { desc = "Escape to Normal Mode" })
+map("n", "<leader>Q", "<cmd>qa<cr>", { desc = "Quit All" })
 
 -- Copilot
 map("i", "<C-CR>", "<cmd>lua vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')<CR>", { desc = "Copilot Accept" })
@@ -24,3 +24,19 @@ map("n", "<leader>ql", "<cmd>lua require('persistence').load({ load = true })<cr
 
 -- conform (already default)
 -- map("n", "<leader>fm", "<cmd>lua require('conform').format()<cr>", { desc = "Format file" })
+
+-- gitsigns
+map("n", "<leader>gb", "<cmd>lua require('gitsigns').blame_line()<cr>", { desc = "Git - Blame Line" })
+map("n", "<leader>tb", "<cmd>lua require('gitsigns').toggle_current_line_blame()<cr>", { desc = "Toggle Git Blame" })
+map("n", "<leader>gd", "<cmd>lua require('gitsigns').diffthis('~1')<cr>", { desc = "Git - Diff This" })
+map("n", "<leader>gs", "<cmd>lua require('gitsigns').stage_hunk()<cr>", { desc = "Git - Stage Hunk" })
+map("n", "<leader>gu", "<cmd>lua require('gitsigns').undo_stage_hunk()<cr>", { desc = "Git - Undo Stage Hunk" })
+map("n", "<leader>gr", "<cmd>lua require('gitsigns').reset_hunk()<cr>", { desc = "Git - Reset Hunk" })
+map("n", "<leader>gP", "<cmd>lua require('gitsigns').preview_hunk()<cr>", { desc = "Git - Preview Hunk" })
+map("n", "<leader>gn", "<cmd>lua require('gitsigns').next_hunk()<cr>", { desc = "Git - Next Hunk" })
+map("n", "<leader>gp", "<cmd>lua require('gitsigns').previous_hunk()<cr>", { desc = "Git - Previous Hunk" })
+map("n", "<leader>gn", "<cmd>lua require('gitsigns').next_hunk()<cr>", { desc = "Git - Next Hunk" })
+map("n", "<leader>gp", "<cmd>lua require('gitsigns').previous_hunk()<cr>", { desc = "Git - Previous Hunk" })
+map("n", "<leader>gS", "<cmd>lua require('gitsigns').stage_buffer()<cr>", { desc = "Git - Stage Buffer" })
+map("n", "<leader>gR", "<cmd>lua require('gitsigns').reset_buffer()<cr>", { desc = "Git - Reset Buffer" })
+map("n", "<leader>td", "<cmd>lua require('gitsigns').toggle_deleted()<cr>", { desc = "Git - Toggle Deleted" })
