84557ba5af0473bbc5e6ad15717bbebae1f01489
Duarte Rocha <duarte.rocha@kelvininc.com>
2025-03-31 16:54:09 +0100

    nvim: added relative path and file type to statusline

--- neovim/lua/chadrc.lua
+++ neovim/lua/chadrc.lua
@@ -9,9 +9,44 @@ M.base46 = {
   theme_toggle = { "tokyodark", "one_light" },
 }
 
+-- helper function to get the current buffer
+local stbufnr = function()
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+end
+
 M.ui = {
   tabufline = {
     lazyload = false,
+  },
+  statusline = {
+    -- default = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    -- vscode = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
+    order = {
+      "mode",
+      "file",
+      "ft",
+      "git",
+      "%=",
+      "lsp_msg",
+      "%=",
+      "diagnostics",
+      "lsp",
+      "cwd",
+      "relativepath",
+      "cursor",
+    },
+    modules = {
+      relativepath = function()
+        local path = stbufnr()
+        if path == "" then
+          return ""
+        end
+        return "/%#St_cwd_text#" .. vim.fn.expand "%:.:h" .. " "
+      end,
+
+      ft = "%#St_file#" .. " %y ",
+      f = "%f",
+    },
   },
 }
 
